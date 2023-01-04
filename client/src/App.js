c2a683c4f841bcab8d14b779af8078a02f07a8b9
Mohammed Safwan <safwanpklr@gmail.com>
2023-01-04 22:04:24 +0530

    Second Day

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux'
 
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from './images/memories.png'
+import useStyles from './styles'
+
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+
+    }, [])
+
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit" >
-                <Typography variant="h2" align="center"> Memories </Typography>
-                <img src={memories} alt="memories" height="60"/>
+            <AppBar className={classes.appBar} position="static" color="inherit" >
+                <Typography className={classes.heading} variant="h2" align="center"> Memories </Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} lg={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} lg={4}>
+                            <Form />
+                        </Grid>
 
                     </Grid>
                 </Container>
